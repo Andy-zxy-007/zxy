7058d4c9f451d5bdcd0ce6bba274f41e852d1d37
Andy-zxy-007 <zhangxiaoyuisgood@163.com>
2023-03-12 09:58:31 +1030

    modify question08.sh

--- question08.sh
+++ question08.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd /root/zxy
-mkdir /root/zxy/dir2
-mv /root/zxy/*.txt  /root/zxy/dir2
-git add -A /root/zxy/dir2
-git commit -m "submitting change dir2"
-git push
+mkdir dir2
+mv *.txt  dir2
+git add -A dir2/*.txt
+git commit -m "submitting change dir2*.txt"
