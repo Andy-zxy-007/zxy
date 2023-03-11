2f7c02e2213c1c117d8cf4999c562f46b07fd639
Andy-zxy-007 <zhangxiaoyuisgood@163.com>
2023-03-12 10:16:29 +1030

    modify question11.sh

--- question11.sh
+++ question11.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 cd /root/zxy
 git checkout -b branch2
-touch /root/zxy/file4
-git add /root/zxy/file4
+touch file4
+git add file4
 git commit -m "add file4 to branch2"
 
 echo "hello world" >> file4
 git stash
 git checkout main
-
