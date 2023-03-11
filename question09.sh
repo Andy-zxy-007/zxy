9a99ed48e8459f56f53189bfdb17c269657e48a2
Andy-zxy-007 <zhangxiaoyuisgood@163.com>
2023-03-12 10:14:14 +1030

    modify question09.sh

--- question09.sh
+++ question09.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 cd /root/zxy
-git branch branch1 
-#git push --set-upstream origin branch1
-git checkout branch1
-touch /root/zxy/file3
-git add /root/zxy/file3
+git checkout -b branch1
+touch file3
+git add file3
 git commit -m "submitting change file3"
-#git push
