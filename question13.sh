59caa98fe9ab36b6dd8a065052e0067fb3dd2a9b
Andy-zxy-007 <zhangxiaoyuisgood@163.com>
2023-03-12 11:03:12 +1030

    modify question13.sh

--- question13.sh
+++ question13.sh
@@ -2,12 +2,8 @@
 cd /root/zxy
 git checkout -b branch3
 git rm *.sh
-
 touch file13.txt
-
 git add file13.txt
-
 git commit -m "add file13.txt to branch3"
-
 git push --set-upstream origin branch3
 
