4ff97a1595d472fd0d9effb1f1dbc6e830eb5f79
Andy-zxy-007 <zhangxiaoyuisgood@163.com>
2023-03-12 10:38:32 +1030

    modify question16.sh

--- question16.sh
+++ question16.sh
@@ -6,9 +6,8 @@ git branch -a
 git checkout branch2
 git merge branch3
 git status
-
 git add .
 git commit -m "Merged branch3 with branch2"
 git branch -d branch3
-git push origin branch2
+#git push origin branch2
 
