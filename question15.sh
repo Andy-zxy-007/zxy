8dbbd4c3d05682300bcebca39d9348492b56744d
Andy-zxy-007 <zhangxiaoyuisgood@163.com>
2023-03-12 10:34:10 +1030

    modify question15.sh

--- question15.sh
+++ question15.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 git clone https://github.com/ian-knight-uofa/git-practice-01.git
-cd git-practice-01
-git branch -a
-git checkout main
-git merge branch1
-git status
+cd git-practice-01    # navigate to the repository directory
+git checkout branch1  # switch to branch1
+git merge main        # merge main into branch1
 
-git add .
-git commit -m "Merged branch1 with main"
-git push origin main
+mkdir merged-files         # create a new directory to store merged files
+cp file1.txt merged-files/ # copy merged file1.txt to merged-files directory
+cp file2.txt merged-files/ # copy merged file2.txt to merged-files directory
+git add merged-files/      # add merged files to staging area
+git commit -m "Added merged versions of conflicted files"  # commit changes
