db205e2a59485914197327b8ecc68d748d8c8779
Andy-zxy-007 <zhangxiaoyuisgood@163.com>
2023-03-12 12:48:41 +1030

    modify question13.sh

--- question13.sh
+++ question13.sh
@@ -2,8 +2,9 @@
 cd /root/zxy
 git checkout -b branch3
 git rm *.sh
+git commit -m "Removing *.sh files"
 touch file13.txt
 git add file13.txt
 git commit -m "add file13.txt to branch3"
-git push --set-upstream origin branch3
+git push -u origin branch3
 
